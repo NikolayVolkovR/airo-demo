6388e0f5a9ff4d347105ba0750f29d5bddf6b559
NikolayVolkovR <n.n.volkov@mail.ru>
2019-09-02 16:19:37 +0300

    Create product.vue

---
+++ src/components/Pages/demo/airo/orders/create/products-list/product/product.vue
@@ -0,0 +1,36 @@
+<template>
+    <article class="product product_theme_list">
+        <div class="product__name text_centered">{{product.name}}</div>
+        <div class="product__quantity text_centered">{{product.quantity}} шт </div>
+        <div class="product__price text_centered">{{product.price * product.quantity}} руб.</div>
+    </article>
+</template>
+
+<script>
+    export default {
+        props: {
+            product: Object
+        }
+    }
+</script>
+
+<style lang="stylus">
+    .product_theme_list
+        display: flex;
+        justify-content: space-around;
+        margin: 5px 0;
+
+        .product__name
+            flex-basis 30%
+            max-width 100px
+        .product__quantity
+            flex-basis 30%
+            max-width 60px
+        .product__price
+            flex-basis 30%
+            max-width 100px
+
+    .text_centered {
+
+    }
+</style>
