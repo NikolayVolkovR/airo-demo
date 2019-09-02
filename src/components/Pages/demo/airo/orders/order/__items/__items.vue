8a82fac6d9b1571a00d909cd4ece0cd7ce651dbb
NikolayVolkovR <n.n.volkov@mail.ru>
2019-09-02 16:16:45 +0300

    Create __items.vue

---
+++ src/components/Pages/demo/airo/orders/order/__items/__items.vue
@@ -0,0 +1,25 @@
+<template>
+    <td class="order__items order__items_table-cell">
+        <ul
+                v-for="item in items"
+        >
+            <li class="order-item__name">
+                {{ item.name }} - {{ item.quantity }}шт. {{ parseFloat(item.price) }}руб.
+            </li>
+        </ul>
+    </td>
+</template>
+
+<script>
+    export default {
+        props: {
+            items: Array
+        }
+    }
+</script>
+
+<style lang="stylus">
+    .order__items_table-cell
+        min-width 180px
+
+</style>
