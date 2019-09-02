b581205559135858096c63a5cf7f7b4d4df2619c
NikolayVolkovR <n.n.volkov@mail.ru>
2019-09-02 16:19:12 +0300

    Create productsList.vue

---
+++ src/components/Pages/demo/airo/orders/create/products-list/productsList.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="products-list products-list_theme_order-create">
+        <product
+                v-for="product in products"
+                :key="product.id"
+                :product="product"
+        />
+    </section>
+</template>
+
+<script>
+    import product from './product/product.vue'
+
+    export default {
+        props: {
+            products: Array
+        },
+        components: {
+            product
+        }
+    }
+</script>
+
+<style lang="stylus">
+    .products-list_theme_order-create
+        margin: 20px auto
+
+</style>
