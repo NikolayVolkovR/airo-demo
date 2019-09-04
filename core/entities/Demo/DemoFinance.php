7b0ef398ea2b8f1c7416a2a51559674ba95bb234
NikolayVolkovR <n.n.volkov@mail.ru>
2019-09-04 20:07:38 +0300

    Create DemoFinance.php

---
+++ core/entities/Demo/DemoFinance.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace core\entities\Demo;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%demo_finance}}".
+ *
+ * @property int $id
+ * @property string $email Email
+ * @property string $name ФИО
+ */
+class DemoFinance extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%demo_finance}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['email', 'name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'email' => 'Email',
+            'name' => 'Name',
+        ];
+    }
+}
